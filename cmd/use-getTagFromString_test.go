db40a4b2e650d9a6bd57bc7edb08287e56b09442
irkode <irkode@rikode.de>
2024-12-03 08:01:27 +0100

    cmd/all: add cmdline arg to install/use a specific tag (obsoleting --latest flag)

---
+++ cmd/use-getTagFromString_test.go
@@ -0,0 +1,101 @@
+package cmd
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
+
+func newMockRepo(reverse bool) *repository {
+	r := repository{
+		tags:      []string{"v1.2.3", "v1.2.2", "v1.2.1", "v1.2.0", "v1.1.2", "v1.1.1", "v1.1.0", "v1.0.0", "v0.2.2", "v0.2.1", "v0.2.0", "v0.1.2", "v0.1.1", "v0.1.0"},
+		latestTag: "v1.2.3",
+	}
+	if reverse {
+		slices.Reverse(r.tags)
+	}
+	return &r
+}
+
+var testCases = []struct {
+	given string
+	want  string
+}{
+	// invalid strings
+	{"", ""},
+	{"abc", ""},
+	{"a.b", ""},
+	{"v1.", ""},
+	{"v1.2.", ""},
+	{"v1.2.3.", ""},
+	{"v1.2.3.4", ""},
+	{"1.", ""},
+	{"1.2.", ""},
+	{"1.2.3.", ""},
+	{"1.2.3.4", ""},
+	{"late", ""},
+	// valid strings
+	{"latest", "v1.2.3"},
+	{"v", "v1.2.3"},
+	{"v1", "v1.2.3"},
+	{"v1.2", "v1.2.3"},
+	{"v1.2.3", "v1.2.3"},
+	{"1", "v1.2.3"},
+	{"1.2", "v1.2.3"},
+	{"1.2.3", "v1.2.3"},
+	// major only use lookup
+	{"0", "v0.2.2"},
+	{"v0", "v0.2.2"},
+	{"1", "v1.2.3"},
+	{"v1", "v1.2.3"},
+	{"v1", "v1.2.3"},
+	// major and minor use lookup
+	{"0.1", "v0.1.2"},
+	{"v0.1", "v0.1.2"},
+	{"1.1", "v1.1.2"},
+	{"v1.1", "v1.1.2"},
+	{"0.2", "v0.2.2"},
+	{"v0.2", "v0.2.2"},
+	// latest-major, minor and lookup)
+	{".1", "v1.1.2"},
+	{"v.1", "v1.1.2"},
+	{".0", "v1.0.0"},
+	{"v.0", "v1.0.0"},
+	// Specific major taken from latestTag (no lookup)
+	{".2.1", "v1.2.1"},
+	{"v.2.1", "v1.2.1"},
+	{".2.1", "v1.2.1"},
+	{"v.2.1", "v1.2.1"},
+}
+
+func TestGetTagFromStringDescending(t *testing.T) {
+	// mock needed elements and sort config
+	Config.SortAscending = false
+	repo := newMockRepo(Config.SortAscending)
+	asset := newAsset()
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("[%s] ShouldBe [%s]", tc.given, tc.want), func(t *testing.T) {
+			asset.tag = ""
+			err := repo.getTagFromString(asset, tc.given)
+			if asset.tag != tc.want {
+				t.Fatalf("given(%s) -> calculated(%s) -> want(%s) : %s", tc.given, asset.tag, tc.want, err)
+			}
+		})
+	}
+}
+
+func TestGetTagFromStringAscending(t *testing.T) {
+	// mock needed elements and sort config
+	Config.SortAscending = true
+	repo := newMockRepo(Config.SortAscending)
+	asset := newAsset()
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("[%s] ShouldBe [%s]", tc.given, tc.want), func(t *testing.T) {
+			asset.tag = ""
+			err := repo.getTagFromString(asset, tc.given)
+			if asset.tag != tc.want {
+				t.Errorf("given(%s) -> calculated(%s) -> want(%s) : %s", tc.given, asset.tag, tc.want, err)
+			}
+		})
+	}
+}
--- cmd/use.go
+++ cmd/use.go
@@ -40,21 +40,32 @@ import (
 
 // useCmd represents the use command
 var useCmd = &cobra.Command{
-	Use:     "use",
+	Use:     "use [flags] [version]",
 	Aliases: []string{"get"},
 	Short:   "Select a version to use in the current directory",
 	Long: `Displays a list of recent Hugo releases, prompting you to select a version
 to use in the current directory. It then downloads, extracts, and caches the
 release asset for your operating system and architecture and writes the version
-tag to an .hvm file.`,
+tag to an .hvm file.
+
+To bypass the selection screen provide a version argument to the command.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		version := ""
+
 		useVersionInDotFile, err := cmd.Flags().GetBool("useVersionInDotFile")
 		cobra.CheckErr(err)
 
-		useLatest, err := cmd.Flags().GetBool("latest")
+		if useVersionInDotFile {
+			version, err = getVersionFromDotFile()
 		cobra.CheckErr(err)
+			if version == "" {
+				cobra.CheckErr(fmt.Errorf("the current directory does not contain an .hvm file"))
+			}
+		} else if len(args) > 0 {
+			version = args[0]
+		}
 
-		err = use(useVersionInDotFile, useLatest)
+		err = use(version)
 		cobra.CheckErr(err)
 	},
 }
@@ -62,7 +73,6 @@ tag to an .hvm file.`,
 func init() {
 	rootCmd.AddCommand(useCmd)
 	useCmd.Flags().Bool("useVersionInDotFile", false, "Use the version specified by the "+App.DotFileName+" file\nin the current directory")
-	useCmd.Flags().Bool("latest", false, "Use the latest version")
 }
 
 // A repository is a GitHub repository.
@@ -85,31 +95,11 @@ type asset struct {
 }
 
 // use sets the version of the Hugo executable to use in the current directory.
-func use(useVersionInDotFile bool, useLatest bool) error {
+func use(version string) error {
 	asset := newAsset()
 	repo := newRepository()
 
-	if useVersionInDotFile {
-		version, err := getVersionFromDotFile(App.DotFilePath)
-		if err != nil {
-			return err
-		}
 		if version == "" {
-			fmt.Fprintln(os.Stderr, "Error: the current directory does not contain an "+App.DotFileName+" file")
-			os.Exit(1)
-		}
-		if !slices.Contains(repo.tags, version) {
-			theFix := fmt.Sprintf("run \"%[1]s use\" to select a version, or \"%[1]s disable\" to remove the file", App.Name)
-			fmt.Fprintf(os.Stderr, "Error: the version specified in the "+App.DotFileName+" file (%s) is not available in the repository: %s\n", version, theFix)
-			os.Exit(1)
-		}
-		asset.tag = version
-	} else if useLatest {
-		err := repo.getLatestTag(asset)
-		if err != nil {
-			return err
-		}
-	} else {
 		msg := "Select a version to use in the current directory"
 		err := repo.selectTag(asset, msg)
 		if err != nil {
@@ -117,6 +107,11 @@ func use(useVersionInDotFile bool, useLatest bool) error {
 		}
 		if asset.tag == "" {
 			return nil // the user did not select a tag; do nothing
+		}
+	} else {
+		err := repo.getTagFromString(asset, version)
+		if err != nil {
+			return err
 		}
 	}
 
@@ -263,6 +258,59 @@ func (r *repository) getLatestTag(a *asset) error {
 		return fmt.Errorf("no latest release found")
 	}
 	a.tag = r.latestTag
+	return nil
+}
+
+func (r *repository) getTagFromString(a *asset, version string) error {
+	inputVersion := version
+	// fast return for simple cases
+	switch version {
+	case "latest", "v":
+		return r.getLatestTag(a)
+	}
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
+	}
+	if strings.HasPrefix(version, "v.") { // empty major
+		version = strings.Replace(version, "v.", semver.Major(r.latestTag)+".", 1)
+	}
+	if semver.Compare(version, "") == 0 {
+		return fmt.Errorf("invalid tag: %s", inputVersion)
+	}
+	if version == semver.Canonical(version) { // major.minor.patch
+		a.tag = version
+	} else {
+		type CompareSemVer func(semver string) bool
+		var compareSemVer CompareSemVer
+		if version == semver.Major(version) { // major only
+			compareSemVer = func(t string) bool {
+				return semver.Compare(semver.Major(version), semver.Major(t)) >= 0
+			}
+		} else if version == semver.MajorMinor(version) { // major.minor
+			compareSemVer = func(t string) bool {
+				return semver.Major(version) == semver.Major(t) && semver.Compare(semver.MajorMinor(version), semver.MajorMinor(t)) >= 0
+			}
+		}
+		if Config.SortAscending {
+			for i := len(r.tags) - 1; i >= 0; i-- {
+				tag := r.tags[i]
+				if compareSemVer(tag) {
+					a.tag = tag
+					return nil
+				}
+			}
+		} else {
+			for _, tag := range r.tags {
+				if compareSemVer(tag) {
+					a.tag = tag
+					return nil
+				}
+			}
+		}
+	}
+	if !slices.Contains(r.tags, a.tag) {
+		return fmt.Errorf("tag \"%s\" not found in repository", inputVersion)
+	}
 	return nil
 }
 
